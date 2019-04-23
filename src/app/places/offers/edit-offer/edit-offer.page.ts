58fde9712072cb6f2ce76b0dec8efce6c545fd3f
scott <scotty.wright30gmail.com>
2019-04-23 23:12:45 +0100

    Added create and update offer logic

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Place } from '../../places.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PlacesService } from '../../places.service';
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController } from '@ionic/angular';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-offer',
@@ -14,13 +15,15 @@ import { Subscription } from 'rxjs';
 export class EditOfferPage implements OnInit, OnDestroy {
   place: Place;
   form: FormGroup;
-  private placeSub: Subscription;
+  private destroyed$ = new Subject();
 
   constructor(
     private route: ActivatedRoute,
     private placesService: PlacesService,
     private navCtrl: NavController,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -29,8 +32,9 @@ export class EditOfferPage implements OnInit, OnDestroy {
         this.navCtrl.navigateBack('/places/tabs/offers');
         return;
       }
-      this.placeSub = this.placesService
+      this.placesService
         .find(paramMap.get('placeId'))
+        .pipe(takeUntil(this.destroyed$))
         .subscribe(place => {
           this.place = place;
           this.setupForm();
@@ -40,14 +44,37 @@ export class EditOfferPage implements OnInit, OnDestroy {
 
   setupForm() {
     this.form = this.formBuilder.group({
-      title: ['', { validators: [Validators.email, Validators.required], updateOn: 'blur' }],
-      description: ['', { validators: [Validators.minLength(6), Validators.required], updateOn: 'blur' }],
+      title: [this.place.title, { validators: [Validators.required], updateOn: 'blur' }],
+      description: [this.place.description, { validators: [Validators.required, Validators.maxLength(180)], updateOn: 'blur' }],
     });
   }
 
+  async onUpdateOffer() {
+    if (!this.form.valid) {
+      return;
+    };
+
+    const loadingEl = await this.loadingCtrl
+      .create({
+        message: 'Updating place...'
+      })
+    loadingEl.present();
+    this.placesService
+      .updatePlace(
+        this.place.id,
+        this.form.value.title,
+        this.form.value.description
+      )
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(() => {
+        loadingEl.dismiss();
+        this.form.reset();
+        this.router.navigate(['/places/tabs/offers']);
+      });
+  }
+
   ngOnDestroy(): void {
-    if (this.placeSub) {
-      this.placeSub.unsubscribe();
-    }
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }
--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -41,29 +41,27 @@ export class NewOfferPage implements OnInit {
     });
   }
 
-  onCreateOffer() {
+  async onCreateOffer() {
     if (!this.form.valid) {
       return;
     }
-    this.loadingCtrl
+    const loadingEl = await this.loadingCtrl
       .create({
         message: 'Creating place...'
-      })
-      .then(loadingEl => {
-        loadingEl.present();
-        this.placesService
-          .addPlace(
-            this.form.value.title,
-            this.form.value.description,
-            +this.form.value.price,
-            new Date(this.form.value.dateFrom),
-            new Date(this.form.value.dateTo)
-          )
-          .subscribe(() => {
-            loadingEl.dismiss();
-            this.form.reset();
-            this.router.navigate(['/places/tabs/offers']);
-          });
+      });
+    loadingEl.present();
+    this.placesService
+      .addPlace(
+        this.form.value.title,
+        this.form.value.description,
+        +this.form.value.price,
+        new Date(this.form.value.dateFrom),
+        new Date(this.form.value.dateTo)
+      )
+      .subscribe(() => {
+        loadingEl.dismiss();
+        this.form.reset();
+        this.router.navigate(['/places/tabs/offers']);
       });
   }
 }
