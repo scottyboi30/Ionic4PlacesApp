a1cae4f236e9d5bcda8bf53efd3a0220f8ba4984
scott <scotty.wright30gmail.com>
2019-04-24 23:20:22 +0100

    updated filtering

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../places.model';
 import { Subscription } from 'rxjs';
+import { SegmentChangeEventDetail } from '@ionic/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-discover',
@@ -12,18 +14,39 @@ export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
   isLoading = false;
+  private chosenFilter = 'all';
   private placesSub: Subscription;
+  relevantPlaces: Place[];
 
-  constructor(public placesService: PlacesService) { }
+  constructor(public placesService: PlacesService, private authService: AuthService) { }
 
   ngOnInit() {
     this.placesSub = this.placesService.places.subscribe(places => {
       this.loadedPlaces = places;
-      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+      if (this.chosenFilter === 'all') {
+        this.relevantPlaces = this.loadedPlaces;
+        this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      } else {
+        this.relevantPlaces = this.loadedPlaces.filter(
+          place => place.userId !== this.authService.userId
+        );
+        this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      }
     });
   }
 
-  onFilterChange(event) {
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    if (event.detail.value === 'all') {
+      this.relevantPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      this.chosenFilter = 'all';
+    } else {
+      this.relevantPlaces = this.loadedPlaces.filter(
+        place => place.userId !== this.authService.userId
+      );
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      this.chosenFilter = 'bookable';
+    }
   }
 
   ngOnDestroy(): void {
